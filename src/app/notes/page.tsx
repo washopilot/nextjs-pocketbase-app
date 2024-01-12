4149dc5d568f5391211180f1df98bd8a49042eb1
Fernando Chicaiza <washopilot@yahoo.com>
2024-01-11 21:03:14 -0500

    add notes route

---
+++ src/app/notes/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotesPage = () => {
+    return (
+        <div>
+            <h1>Notes Page</h1>
+        </div>
+    )
+}
+
+export default NotesPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <>
+        <div>
             <h1>Home Page</h1>
             <p>Some content</p>
-        </>
+        </div>
     )
 }
 
